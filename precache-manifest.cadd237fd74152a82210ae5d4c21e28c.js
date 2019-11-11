b431cd84e1459c7a7cd713fcb402de5a877cc31e
Shashank Kotturi <shashank.kotturi@edu.uwaterloo.ca>
2019-11-11 14:55:03 -0500

    Updates

---
+++ precache-manifest.cadd237fd74152a82210ae5d4c21e28c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80f9f415624ebe7e24ef6a99b0bebb97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b891b305d1c3d0bd5fbf",
+    "url": "/static/css/2.f30c198b.chunk.css"
+  },
+  {
+    "revision": "b655ca2a34315e4b361a",
+    "url": "/static/css/main.d986c45f.chunk.css"
+  },
+  {
+    "revision": "b891b305d1c3d0bd5fbf",
+    "url": "/static/js/2.aaa94a19.chunk.js"
+  },
+  {
+    "revision": "b655ca2a34315e4b361a",
+    "url": "/static/js/main.5b8d4d68.chunk.js"
+  },
+  {
+    "revision": "94c64b5467b6c7e87d35",
+    "url": "/static/js/runtime-main.00a199b9.js"
+  },
+  {
+    "revision": "74f5efa915053dfe8a154fcd8d987d04",
+    "url": "/static/media/logo.74f5efa9.png"
+  },
+  {
+    "revision": "6ea0b1b47b5f0811a76d4e636a84dae9",
+    "url": "/static/media/profile.6ea0b1b4.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.914ddd30979bdfe11ef532bd29c795d9.js"
+  "/precache-manifest.cadd237fd74152a82210ae5d4c21e28c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
