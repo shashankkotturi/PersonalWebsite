27cc0e166dec5d6eeb5b700283499f25cf896456
Shashank Kotturi <shashank.kotturi@edu.uwaterloo.ca>
2019-11-11 08:36:29 -0500

    Updates

---
+++ precache-manifest.6041ee2113cd48f0d5f89320941c5ef8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2b2e54fcc29c1843779013f661793e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b891b305d1c3d0bd5fbf",
+    "url": "/static/css/2.f30c198b.chunk.css"
+  },
+  {
+    "revision": "461957ae97b8e75a3be8",
+    "url": "/static/css/main.d986c45f.chunk.css"
+  },
+  {
+    "revision": "b891b305d1c3d0bd5fbf",
+    "url": "/static/js/2.aaa94a19.chunk.js"
+  },
+  {
+    "revision": "461957ae97b8e75a3be8",
+    "url": "/static/js/main.3221d124.chunk.js"
+  },
+  {
+    "revision": "94c64b5467b6c7e87d35",
+    "url": "/static/js/runtime-main.00a199b9.js"
+  },
+  {
+    "revision": "74f5efa915053dfe8a154fcd8d987d04",
+    "url": "/static/media/logo.74f5efa9.png"
+  },
+  {
+    "revision": "6ea0b1b47b5f0811a76d4e636a84dae9",
+    "url": "/static/media/profile.6ea0b1b4.jpg"
+  }
+]);
