932ff13f8e4550c30f1f0b9eb6a0045b15766a60
shashankkotturi <shashankkotturi@outlook.com>
2022-01-02 15:07:32 -0800

    Updates

---
+++ precache-manifest.b5e4a4f378d8cb89c8f5a1c5d01886a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bd49da01e193c57f2b9b0aede93feff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a923a1883477b2161721",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "ea2927661c7b122e9c3c",
+    "url": "/static/js/2.b0640829.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b0640829.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a923a1883477b2161721",
+    "url": "/static/js/main.2248234f.chunk.js"
+  },
+  {
+    "revision": "00c5ffef6a2f2a23cd12",
+    "url": "/static/js/runtime-main.a6ebd9d1.js"
+  }
+]);
